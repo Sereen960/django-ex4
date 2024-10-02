02007218e26ec2c235ee721755b0d4e4bda47413
sereen <146604026+Sereen960@users.noreply.github.com>
2024-10-02 15:31:38 +0530

    Add files via upload

---
+++ ex4/myapp/views.py
@@ -0,0 +1,14 @@
+# email_app/views.py
+from django.core.mail import send_mail
+from django.conf import settings
+from django.shortcuts import render
+
+def send_email(request):
+    send_mail(
+        'Welcome Party - reg.',
+        'Greetings!!! Welcome to the Department of Information Technology.',
+        settings.EMAIL_HOST_USER,
+        ['sereenfathima86@gmail.com'],
+        fail_silently=False,
+    )
+    return render(request, 'sendMail.html')
